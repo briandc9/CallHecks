aa1e311e733ae21bae0c9fefceff6fe03c64612e
Brian Chin <bchin821@berkeley.edu>
2018-11-03 13:23:57 -0700

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,24 +1,40 @@
-class Calendar():
-	def _init_(self, location):
-		self.events = events
-		self.location = location
+import random
+class Calendar:
+    def _init_(self, location):
+        self.events = {}
+        self.location = location
         
-class Event(object):
-    def _init_(self, name, genre, date, time, description, price):
+    def addEvent(self, event):
+        self.events[(event.name, event.date)] = event
+        
+        
+class Event:
+    def _init_(self, name, genre, date, description, price):
         self.name = name
         self.genre = genre
         self.date = date
-        self.time = time
         self.description = description
         self.price = price
         self.all_artists = set()
         self.approved_artists = {}
         self.unapproved_artists = set()
         
+    def register(self, artist):
+        self.unapproved_artists.add(artist)
+        
     def vote(self, artist):
         self.approved_artists[artist.name] += 1
+        
+    def unvote(self, artist):
+        self.approved_artists[artist.name] -= 1
     
     def approve(self, artist):
         self.unapproved_artists.remove(artist)
         self.approved_artists[artist.name] = 0
+        
+    def finalizeArtist(self):
+        maximum = max(self.approved_artists.items())
+        inverse = [self.approved_artists.getKey() for maximum in self.approved_artists.items()]
+        return inverse[random.randInt(0, inverse.length)]
     
+    def 

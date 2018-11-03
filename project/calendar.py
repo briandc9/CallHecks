14bc68da37e4edfc79680bd3b62ffbf42c8ba267
Brian Chin <bchin821@berkeley.edu>
2018-11-03 14:42:12 -0700

    Update project/calendar.py

--- project/calendar.py
+++ project/calendar.py
@@ -16,12 +16,13 @@ class Calendar:
 
 
 class Event:
-    def _init_(self, name, genre, date, description, price):
+    def _init_(self, name, genre, date, description, price, timeslot):
         self.name = name
         self.genre = genre
         self.date = date
         self.description = description
         self.price = price
+        self.timeslot = timeslot
         self.all_artists = set()
         self.approved_artists = {}
         self.unapproved_artists = set()
@@ -47,3 +48,26 @@ class Event:
         if type(other) is not Event:
             return False
         return self.name == other.name and self.date == other.date
+    
+    def isOverlap(self, event):
+        matchday = datetime.event.date.today()
+        for key in self.events:
+            priordate = datetime.self.events[key].date.today()
+            if (priordate.equals(matchday)):
+                mminutes = matchday*60 + matchday.minute
+                pminutes = priordate.hour*60 + priordate.minute
+                if (pminutes < mminutes and (pminutes + timeslot > mminutes)):
+                    return False
+                if (mminutes < pminutes and (mminutes + timeslot > pminutes)):
+                    return False
+        return True
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+            

d4f45f2f32e185d461cbd7ef29e3221df4ae047a
Brian Chin <bchin821@berkeley.edu>
2018-11-03 12:39:36 -0700

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -4,12 +4,22 @@ class Calendar():
 		self.location = location
         
 class Event(object):
-    def _init_(self, name, genre, date, time, description, price, unapproved_artists={}, approved_artists={}, rejected_artists={}):
+    def _init_(self, name, genre, date, time, description, price, all_artists = {}, unapproved_artists={}, approved_artists={}):
         self.name = name
         self.genre = genre
         self.date = date
         self.time = time
-        self.unapproved_artists = unapproved_artists
+        self.all_artists = all_artists
         self.approved_artists = approved_artists
-        self.rejected_artists = rejected_artists
-    def addUnapproved(
+    def addUnapproved(artist):
+        if (artist.name in self.all_artists):
+            print (artist.name + 'has already applied to this venue.')
+            return
+        else:
+            
+    def vote(artist):
+    
+    def approve(artist):
+        
+        
+        

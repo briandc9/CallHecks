850d660747b567db44eb83dd9a50e4e514ca85ed
orange-sunshine <spvengat@gmail.com>
2018-11-04 04:00:36 -0800

    login and profile system implemented

--- venuebooker/land/urls.py
+++ venuebooker/land/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #path('route', view.function, name='booker-something'),
     path('', views.home, name='booker-home'),
     path('about', views.about, name='booker-about'),
+
 ]
